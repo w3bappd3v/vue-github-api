2038da3c7f385af3c52f398f30675681e352db79
Rob R <r0brh0d3s@gmail.com>
2020-12-22 10:50:55 -0800

    readme and cleanup

--- src/components/Repository.vue
+++ src/components/Repository.vue
@@ -11,25 +11,25 @@
 </script>
 
 <template>
-    <div class="repository">
-      <h3>{{ repository.name }}</h3>
-      <div class="info-row">
-        <div class="owner">
-          <h4>{{ repository.owner.login}}</h4>
-          <img :src="repository.owner.avatar_url" class="owner-avatar" />
-        </div>
-        <div class="languages">
-          <h4>Stats</h4>
-          <ul>
-            <li>Language: {{ repository.language }}</li>
-            <li>Forks: {{ repository.forks_count }}</li>
-            <li>Watchers: {{ repository.watchers_count }}</li>
-            <li>Stargazers: {{ repository.stargazers_count }}</li>
-            
-          </ul>
-        </div>
+  <div class="repository">
+    <h3>{{ repository.name }}</h3>
+    <div class="info-row">
+      <div class="owner">
+        <h4>{{ repository.owner.login}}</h4>
+        <img :src="repository.owner.avatar_url" class="owner-avatar" />
+      </div>
+      <div class="languages">
+        <h4>Stats</h4>
+        <ul>
+          <li>Language: {{ repository.language }}</li>
+          <li>Forks: {{ repository.forks_count }}</li>
+          <li>Watchers: {{ repository.watchers_count }}</li>
+          <li>Stargazers: {{ repository.stargazers_count }}</li>
+          
+        </ul>
       </div>
     </div>
+  </div>
 </template>
 l
 <style scoped lang="scss">
